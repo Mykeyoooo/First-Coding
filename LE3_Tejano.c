0eb23f5be40e0f43a1b84019f38e0616959c747e
Mykeyoooo <gragza113@gmail.com>
2025-09-18 08:54:42 +0800

    Add files via upload

---
+++ LE3_Tejano.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    char section[20];
+    float num1, num2;
+
+    // Input name and section
+    printf("Enter Complete Name: ");
+    fgets(name, sizeof(name), stdin); // use fgets to allow spaces in name
+
+    printf("Enter Section: ");
+    fgets(section, sizeof(section), stdin);
+
+    // Input two numbers
+    printf("\nEnter first number: ");
+    scanf("%f", &num1);
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    // Output
+    printf("\nStudent Calculator\n");
+    printf("Student name: %s", name);
+    printf("Section: %s", section);
+
+    printf("Results:\n");
+    printf("%.0f + %.0f = %.0f\n", num1, num2, num1 + num2);
+    printf("%.0f - %.0f = %.0f\n", num1, num2, num1 - num2);
+    printf("%.0f * %.0f = %.0f\n", num1, num2, num1 * num2);
+    printf("%.0f / %.0f = %.2f\n", num1, num2, num1 / num2);
+
+    return 0;
+}
